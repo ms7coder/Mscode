d2af068b5896192b588b7c654a4ca1ac815ff5f3
ms7coder <skinyua222@gmail.com>
2025-02-11 10:53:49 +0300

    Initial commit

---
+++ PLP/PYTHON CODES/index.py
@@ -0,0 +1,22 @@
+# Ask the user for input
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+operation = input("Enter the operation (+, -, *, /): ")
+
+# Perform the operation based on user input
+if operation == "+":
+    result = num1 + num2
+elif operation == "-":
+    result = num1 - num2
+elif operation == "*":
+    result = num1 * num2
+elif operation == "/":
+    if num2 != 0:
+        result = num1 / num2
+    else:
+        result = "Error! Division by zero."
+else:
+    result = "Invalid operation"
+
+# Print the result
+print(f"The result of {num1} {operation} {num2} is: {result}")
